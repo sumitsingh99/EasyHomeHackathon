fd98cfdf8d0bd0b7b6f36261f562bd21b6d093aa
tarunngusain08 <prudent.tarun0808@gmail.com>
2025-04-27 15:17:21 +0530

    adding login: App.tsx

--- PaymentReminder/frontend/src/App.tsx
+++ PaymentReminder/frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import AuthPage from './components/auth/AuthPage';
 import Dashboard from './components/Dashboard';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <div className="app-container">
+      <AuthPage />
+      <Dashboard />;
+    </div>
+  );
 }
 
 export default App;
