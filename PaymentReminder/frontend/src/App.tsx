f4392bee57e7a8675353ee49edb4bf7fb53b67d6
tarunngusain08 <prudent.tarun0808@gmail.com>
2025-04-30 18:55:58 +0530

    :)

--- PaymentReminder/frontend/src/App.tsx
+++ PaymentReminder/frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Dashboard from './components/Dashboard';
 function App() {
   return (
     <div className="app-container">
-      <AuthPage />
+      {/* <AuthPage /> */}
       <Dashboard />;
     </div>
   );
